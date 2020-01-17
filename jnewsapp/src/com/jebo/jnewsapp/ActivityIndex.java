867116e0e9d60b9c62e645021a8f7088acb441d6
John Jebo <johnjebo@gmail.com>
2020-01-17 12:13:02 +0000

    First Commit

---
+++ jnewsapp/src/com/jebo/jnewsapp/ActivityIndex.java
@@ -0,0 +1,50 @@
+package com.jebo.jnewsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityIndex extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+}
